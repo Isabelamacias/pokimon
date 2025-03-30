b922ffb0c5ceb0cb862decf0a57cd4c9aa82b891
isabela <you@example.com>
2025-03-29 23:01:31 -0500

    v7

---
+++ js/registrar.js
@@ -0,0 +1,13 @@
+function Registrar(){
+    let Email = document.getElementById('email').value
+    let password = document.getElementById('contraseña').value
+    let verifique = document.getElementById('confi-contra').value
+
+
+    if(username == 'Rodolfo' && password == '123456'){
+        alert('Usuario valido')
+    }else{
+        alert('usuario incorrecto')
+    }  
+
+}
